f3bacb42f599eba512da404e08d9d77008bb2127
basbk <basbkk1@gmail.com>
2017-08-27 13:20:27 +0300

    fixing[2]

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, request, jsonify
-from pony.orm import db_session, desc
+from pony.orm import db_session, desc, select
 from models import Menu, MenuItem, Thesis, FlowSubscription, Admin, Docs
 from config import bot_token
 import telebot
@@ -117,7 +117,7 @@ def reply(item, message):
         if item.forward_to == Menu['presentation']:
             markup = item.forward_to.get_markup()
             bot.send_message(message.chat.id, item.text, reply_markup=markup)
-            for doc in Docs.order_by(Docs.id):
+            for doc in select(d for d in Docs).order_by(Docs.id):
                 print('ID: ' + str(doc.id))
                 bot.send_document(message.chat.id, doc.file_id)
             return
