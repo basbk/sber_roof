7c0fd0e167923fc483f8c801ebd922f6c0eb19c0
basbk <basbkk1@gmail.com>
2017-08-29 18:35:37 +0300

    Disable links preview

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ def reply(item, message):
                 bot.send_message(message.chat.id, item.text, reply_markup=Menu['speakers'].get_markup())       
                 return        
             markup = item.forward_to.get_markup()
-            bot.send_message(message.chat.id, item.text, reply_markup=markup, parse_mode='Markdown')
+            bot.send_message(message.chat.id, item.text, reply_markup=markup, parse_mode='Markdown', disable_web_page_preview=True)
         else:
             bot.send_message(message.chat.id, item.text)
         if item.video_id is not None and item.video_id is not '':
